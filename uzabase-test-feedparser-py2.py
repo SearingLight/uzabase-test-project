a3eda78ac1bb8af14b82e8b39d6667a4973609c5
Nick Donegan <SearingLight@users.noreply.github.com>
2016-12-12 19:07:28 +0900

    Python 2.7用のファイルをアップロード

---
+++ uzabase-test-feedparser-py2.py
@@ -0,0 +1,17 @@
+#必要モジュールをインポート
+import feedparser
+
+#フィードを選択・挿入
+feed = feedparser.parse('http://tech.uzabase.com/rss')
+feed2 = feedparser.parse('http://www.giantbomb.com/feeds/reviews/')
+feed3 = feedparser.parse('https://www.panic.com/blog/feed/')
+
+#内容を表示
+for post in feed.entries:
+	print post.title[:10].encode("utf-8") + ": " + post.description[:30].encode("utf-8") + ""
+	
+for post in feed2.entries:
+	print post.title[:10].encode("utf-8") + ": " + post.description[:30].encode("utf-8") + ""
+	
+for post in feed3.entries:
+	print post.title[:10].encode("utf-8") + ": " + post.description[:30].encode("utf-8") + ""
